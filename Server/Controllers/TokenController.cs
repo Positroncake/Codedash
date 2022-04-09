b03f536db68125ec3e9543c7ca47130e58897142
roncake <roncake@protonmail.com>
2022-04-09 15:15:39 -0400

    Added dynamic login buttons Added redirect to account page after login/registration

---
+++ Server/Controllers/TokenController.cs
@@ -0,0 +1,26 @@
+using codedash.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace codedash.Server.Controllers;
+
+[ApiController]
+[Route("TokenApi")]
+public class TokenController : ControllerBase
+{
+    private static TokenDbContext _context;
+
+    public TokenController(TokenDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpDelete]
+    [Route("Logout/{token}")]
+    public ActionResult Logout([FromRoute] string token)
+    {
+        List<Token> items = _context.Token!.ToList();
+        foreach (Token item in items.Where(x => x.TokenString!.Equals(token))) _context.Token!.Remove(item);
+        _context.SaveChanges();
+        return Ok();
+    }
+}
