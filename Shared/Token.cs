8bc3b2b2ac15708809d47c1dea20347ad4572962
roncake <roncake@protonmail.com>
2022-04-12 19:53:43 -0400

    Fixing human error Pt 1/2

--- Shared/Token.cs
+++
@@ -1,8 +0,0 @@
-namespace Codedash.Shared;
-
-public class Token
-{
-    public string? Id { get; set; }
-    public string? TokenString { get; set; }
-    public string? UsernameHash { get; set; }
-}
